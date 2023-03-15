c7b45e7d097cf2d4badd7ea5395897716a5a866f
Yasir Khalid <yasir_khalid@outlook.com>
2023-03-15 00:15:58 +0000

    changed make targets to generate requirements.txt file based on src/whykay libs

--- src/whykay/investments/industry_analyzer.py
+++ src/whykay/investments/industry_analyzer.py
@@ -1,7 +1,3 @@
 def calculate_exposure(holdings):
     """calculates industry exposure on the stock holdings"""
-<<<<<<< HEAD
     # TODO
-=======
-    # TODO
->>>>>>> a49278411eaa38fce3d57fb54577e9b3d996108e
